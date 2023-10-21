6724ed556fc8e5925d95d66a762e231ac9d6a19b
mahamtr <mzmaai70@gmail.com>
2023-10-21 11:06:21 +0200

    git rm -r . --cached

--- backend/blobAPI/blobCORE/DTOs/BlobModel.cs
+++
@@ -1,9 +0,0 @@
-namespace blobDATA.DTOs;
-
-public class BlobDto
-{
-    public string? Uri { get; set; }
-    public string? Name { get; set; }
-    public string? ContentType { get; set; }
-    public Stream? Content { get; set; }
-}
--- backend/blobAPI/blobDATA/BlobStorageProvider/IBlobStorageProvider.cs
+++
@@ -1,8 +0,0 @@
-using Azure.Storage.Blobs;
-
-namespace blobDATA.BlobStorageProvider;
-
-public interface IBlobStorageProvider
-{
-    BlobContainerClient GetBLobContainerClient();
-}
